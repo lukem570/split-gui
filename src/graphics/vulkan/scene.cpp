553b9e4ca76e0fb21b095d0cbe8f207892da2205
Luke <94769656+lukem570@users.noreply.github.com>
2025-03-08 01:35:25 -0500

    Fixed sampling issue

--- src/graphics/vulkan/scene.cpp
+++ src/graphics/vulkan/scene.cpp
@@ -61,8 +61,8 @@ namespace SplitGui {
         vk_device.bindImageMemory(vk_scenesImageArrayImages, vk_scenesImageArrayImageMemory, 0);
 
         vk::SamplerCreateInfo samplerInfo;
-        samplerInfo.magFilter               = vk::Filter::eLinear;
-        samplerInfo.minFilter               = vk::Filter::eLinear;
+        samplerInfo.magFilter               = vk::Filter::eNearest;
+        samplerInfo.minFilter               = vk::Filter::eNearest;
         samplerInfo.mipmapMode              = vk::SamplerMipmapMode::eNearest;
         samplerInfo.addressModeU            = vk::SamplerAddressMode::eClampToBorder;
         samplerInfo.addressModeV            = vk::SamplerAddressMode::eClampToBorder;
